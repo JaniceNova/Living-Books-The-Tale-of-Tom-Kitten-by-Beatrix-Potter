db6738895a011602a6ba6d765bb89e54b1023638
JaniceNova <janicenovacek2016@gmail.com>
2018-12-06 08:11:41 -0800

    added sound file

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,9 +1,24 @@
 
-$("#title").hover(function(){
-    $(this).attr("width", $(this).attr("width") * 3.5 )
-    $(this).attr("height", $(this).attr("height") * 3.5 )
+$("#title").hover(function () {
+    $(this).attr("width", $(this).attr("width") * 3.5)
+    $(this).attr("height", $(this).attr("height") * 3.5)
 
-}, function(){
+}, function () {
     $(this).attr("width", $(this).attr("width") / 3.5)
-    $(this).attr("height", $(this).attr("height") / 3.5 )
+    $(this).attr("height", $(this).attr("height") / 3.5)
+})
+
+// button magic --------------
+$(".Buttons").mousedown(function () {
+    $(this).css("background-color", "rgba(37, 87, 129, 0.6)")
+})
+
+$(".Buttons").mouseup(function () {
+    $(this).css("background-color", "rgba(37, 127, 129, 0.6)")
+})
+
+$(".Buttons").hover(function () {
+    $(this).css("background-color", "rgba(37, 127, 129, 0.6)")
+}, function () {
+    $(this).css("background-color", "rgba(0, 0, 0, 0)")
 })
