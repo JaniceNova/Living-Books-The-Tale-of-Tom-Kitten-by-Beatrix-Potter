9ff48beb258e027983c524869be48216f87ef65a
JaniceNova <janicenovacek2016@gmail.com>
2018-12-07 14:58:04 -0800

    got audio to play

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -27,6 +27,37 @@ $(".Buttons").hover(function () {
 
 // audio ---------------------------------------------
 
+$(document).ready(function() {
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', 'assets/javascript/Title.mp3');
+    
+    audioElement.addEventListener('ended', function() {
+        this.play();
+    }, false);
+    
+    // audioElement.addEventListener("canplay",function(){
+    //     $("#length").text("Duration:" + audioElement.duration + " seconds");
+    //     $("#source").text("Source:" + audioElement.src);
+    //     $("#status").text("Status: Ready to play").css("color","green");
+    // });
+    
+    // audioElement.addEventListener("timeupdate",function(){
+    //     $("#currentTime").text("Current second:" + audioElement.currentTime);
+    // });
+    
+    $('#Play').click(function() {
 
-
-$("#audio")[0].play();
+    if()
+        audioElement.play();
+        $("#status").text("Status: Playing");
+    });
+    
+    // $('#pause').click(function() {
+    //     audioElement.pause();
+    //     $("#status").text("Status: Paused");
+    // });
+    
+    // $('#restart').click(function() {
+    //     audioElement.currentTime = 0;
+    // });
+});
