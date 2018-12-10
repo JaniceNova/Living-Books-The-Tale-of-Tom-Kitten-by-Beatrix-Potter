55adb4cacb4648b49f9ee3433cd8c4c6fb032006
JaniceNova <janicenovacek2016@gmail.com>
2018-12-10 09:47:12 -0800

    added modal to question mark

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -93,7 +93,49 @@ $(document).ready(function () {
 });
 
 
+
+
+
 // Code for getting value of span tags
 $("span").mousedown(function () {
     console.log($(this).text())
 })
+
+
+// making span tags
+
+
+
+
+
+
+
+
+
+//model for the questionMark
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("questionMark");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
