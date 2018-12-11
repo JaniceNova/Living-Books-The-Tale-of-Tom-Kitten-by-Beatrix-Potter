72faa3012a46f89d41eb5650700a7c46b99c23ef
JaniceNova <janicenovacek2016@gmail.com>
2018-12-10 22:53:38 -0800

    first page done

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -143,8 +143,38 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+//model for the index
+
+// Get the modal
+var modal2 = document.getElementById('myModal2');
+
+// Get the button that opens the modal
+var btn2 = document.getElementById("index");
+
+// Get the <span> element that closes the modal
+var span2 = document.getElementsByClassName("close2")[0];
+
+// When the user clicks on the button, open the modal 
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+}
 
 
+
+
+//text to speech
 function say(m){ 	var msg = new SpeechSynthesisUtterance(); 	var voices = window.speechSynthesis.getVoices(); 	msg.voice = voices[10]; 	msg.voiceURI = "native"; 	msg.volume = 1; 	msg.rate = 1; 	msg.pitch = 0.8; 	msg.text = m; 	msg.lang = 'en-US';   	speechSynthesis.speak(msg); }
 
 
@@ -164,3 +194,4 @@ $('#invisiblebox2').click(function () {
     
     meow2.play();
 });
+
